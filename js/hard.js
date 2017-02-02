621e0b6812d739a67d6f7e76c29aa4a1f772bee7
Chris Coralli <ccoralli@gmail.com>
2017-02-02 18:34:55 -0500

    question 2 hard mode done.

--- js/hard.js
+++ js/hard.js
@@ -10,3 +10,13 @@ items.forEach(function (object) {
 
 avgPrice = avgPrice.reduce(function (a, b) { return a + b; });
 question1.innerHTML = "The average price is" + " " + "$" + (avgPrice / items.length).toFixed(2);
+
+
+question2 = document.getElementById("answer2");
+
+items.forEach(function(object) {
+    if (object.price < 18 && object.price > 14) {
+        question2.innerHTML += "<p>" + object.title + "</p>";
+    }
+});
+
